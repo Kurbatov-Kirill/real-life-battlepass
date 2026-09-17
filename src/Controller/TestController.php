38c62d0687aaf6665d5e5990a7b37b67f448fc45
Kirill Kurbatov <iil.kurbatov.kirill@gmail.com>
2026-09-17 23:21:44 +0500

    Test deploy

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -10,6 +10,6 @@ class TestController
     #[Route('/')]
     public function greet()
     {
-        return new Response('Hello World Fixed!');
+        return new Response('Hello World!');
     }
 }
