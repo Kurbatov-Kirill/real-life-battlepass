ba6c851fa310ca12c8872ff1b0e0dedb86250c17
Kirill Kurbatov <iil.kurbatov.kirill@gmail.com>
2026-09-21 19:33:22 +0500

    feat: install and test Twig

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,15 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class TestController
+class TestController extends AbstractController
 {
     #[Route('/')]
-    public function greet()
+    public function greet(): Response
     {
-        return new Response('Hello World Fixed!');
+        $message = 'Hello World Fixed!';
+        return $this->render('test/test.html.twig',[
+            'message' => $message
+        ]);
     }
 }
